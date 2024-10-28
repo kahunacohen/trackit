93720b93b2c554e45cc7c2f132e6493d2c581b90
Aaron Cohen <aaroncohendev@gmail.com>
2024-10-28 22:08:05 +0200

    modify description

--- cmd/init.go
+++ cmd/init.go
@@ -18,7 +18,7 @@ import (
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "Initialize tracking.",
+	Short: "Initialize tracking by parsing trackit.yaml and data directory.",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
