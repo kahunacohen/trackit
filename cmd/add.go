2afc12daf418845ca708d8225dadfa8909bb0aaa
Aaron Cohen <aaroncohendev@gmail.com>
2025-01-05 18:10:50 +0200

    add usage

--- cmd/add.go
+++ cmd/add.go
@@ -18,12 +18,8 @@ import (
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Adds transactions",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Adds transactions by parsing CSV files in the data directory. This will
+not parse files whose transactions that already have been added.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		homeDir, _ := os.UserHomeDir()
 		dbPath := filepath.Join(homeDir, "trackit.db")
