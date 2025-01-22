a559fcd7e297bbc6a6153e4c6005e4014c1df14f
Aharon Cohen <aharonc@matav.org.il>
2025-01-22 09:26:01 +0200

    get rate query working

--- internal/db/queries/rates.sql
+++ internal/db/queries/rates.sql
@@ -1,2 +1,7 @@
--- name: ReadRate :one
-SELECT rates.rate FROM rates WHERE currency_codes.symbol = ? AND curr = ?;
+-- name: ReadRateFromSymbols :one
+SELECT 
+    rates.rate 
+FROM rates
+INNER JOIN currency_codes AS from_currency ON from_currency.id = rates.currency_code_from_id
+INNER JOIN currency_codes AS to_currency ON to_currency.id = rates.currency_code_to_id
+WHERE from_currency.symbol =  sqlc.arg(fromSymbol) AND to_currency.symbol =  sqlc.arg(toSymbol);
