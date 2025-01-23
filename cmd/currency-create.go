7a77cd6f6165515e98fa52a8a3e288cbff56c4dc
Aaron Cohen <aaroncohendev@gmail.com>
2025-01-23 19:18:51 +0200

    add currency create

--- cmd/currency-create.go
+++ cmd/currency-create.go
@@ -4,15 +4,35 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
+	"errors"
+	"log"
+	"strings"
+
+	"github.com/kahunacohen/trackit/internal/models"
 	"github.com/spf13/cobra"
 )
 
-// currencyCreateCmd represents the currencyCreate command
 var currencyCreateCmd = &cobra.Command{
 	Use:   "create",
+	Args:  cobra.ExactArgs(1),
 	Short: "Creates a new ISO 4217 currency symbol. trackit currency create ILS",
 	Long:  `Creates a new ISO 4217 three-letter ISO symbol. "USD" is pre-populated. trackit currency create ILS`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args[0]) != 3 {
+			return errors.New("currency symbol must be three characters")
+		}
+		symbol := strings.ToUpper(args[0])
+		db, err := getDB()
+		if err != nil {
+			log.Fatalf("Failed to open database: %v", err)
+		}
+		ctx := context.Background()
+		queries := models.New(db)
+		err = queries.CreateCurrencyCode(ctx, symbol)
+		if err != nil {
+			return err
+		}
 		return nil
 	},
 }
