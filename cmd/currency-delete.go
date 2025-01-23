4a393a70c9b8b9a15e629e92c71d7a1389325002
Aaron Cohen <aaroncohendev@gmail.com>
2025-01-23 19:27:42 +0200

    update

---
+++ cmd/currency-delete.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/kahunacohen/trackit/internal/models"
+	"github.com/spf13/cobra"
+)
+
+// currencyDeleteCmd represents the currencyDelete command
+var currencyDeleteCmd = &cobra.Command{
+	Use:   "delete",
+	Args:  cobra.ExactArgs(1),
+	Short: "Deletes a currency symbol by name",
+	Long:  `Deletes a currency symbol by name. trackit currency delete ILS`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		db, err := getDB()
+		if err != nil {
+			log.Fatalf("Failed to open database: %v", err)
+		}
+		ctx := context.Background()
+		queries := models.New(db)
+		err = queries.DeleteCurrencyCode(ctx, args[0])
+		if err != nil {
+			return fmt.Errorf("error deleting currency code: %w", err)
+		}
+		return nil
+	},
+}
+
+func init() {
+	currencyCmd.AddCommand(currencyDeleteCmd)
+}
