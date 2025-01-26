dc70fac84d4c3f0365807c2874a9b4e603c4f7d4
Aaron Cohen <aaroncohendev@gmail.com>
2025-01-27 00:00:59 +0200

    clean up

--- cmd/root.go
+++ cmd/root.go
@@ -18,13 +18,8 @@ For example:
 
 # initalizes your app by pointing by default to ~/.trackit.yaml
 $ trackit init `,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -33,13 +28,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.trackit.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
